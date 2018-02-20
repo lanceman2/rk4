53e6b1291dc6c90f534fdc1636c3f7075f631f37
Lance Arsenault <lanceman@vt.edu>
2018-02-19 19:50:57 -0500

    Saving Work

---
+++ tests/sine.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "../include/rk4.hpp"
+
+
+class Sin : public RK4<float, float, float>
+{
+    public:
+
+        Sin(float period);
+
+        void setPeriod(float period_in)
+        {
+            period = period_in;
+            angularFreq_2 = 2.0F*M_PI/period;
+            angularFreq_2 *= angularFreq_2;
+            tStep = period/6.0F;
+        };
+
+        float getPeriod(void) { return period; };
+
+    protected:
+
+        void derivatives(float t, const float *x, float *xDot);
+
+    private:
+
+        float angularFreq_2, period;
+};
+
+Sin::Sin(float period):
+    RK4(2/*num degree of freedom*/,
+            0.0F/*time step gets set in setPeriod()*/)
+{
+    setPeriod(period);
+}
+
+void Sin::derivatives(float t, const float *x, float *xDot)
+{
+    xDot[0] = x[1]; // x_dot = v
+    xDot[1] = - angularFreq_2 * x[0];
+}
+
+
+int main(void)
+{
+    float x[2] = { 1.0, 0.0 };
+    float t;
+    Sin sin(1.0F);
+
+    for(t=0.0F; t<10.0F; t += 0.01)
+    {
+        sin.go(x, t);
+        printf("%g %g %g %g\n", t, x[0], x[1], cosf(2.0F*M_PI*t));
+#if 1
+        if(sin.getPeriod() > 5.0F)
+            sin.setPeriod(1.0F);
+        else
+            sin.setPeriod(sin.getPeriod() * 1.003);
+#endif
+    }
+
+    return 0;
+}
